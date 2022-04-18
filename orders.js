c8e39e7427ba8bca2279b4112337a956c2180270
ItsLuvao <lucasmwds@hotmail.com>
2022-04-18 19:11:08 -0300

    First Commit

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'HeadSet gamer',
+        productNumber: '3212',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Mouse Razer 3.0',
+        productNumber: '2112',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Placa de video 1080ti',
+        productNumber: '1123',
+        paymentStatus: 'Due',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'HeadSet Corsair',
+        productNumber: '4432',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GamePed Xbox One',
+        productNumber: '2121',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Teclado Mecanico Razer',
+        productNumber: '9843',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Iphone 11',
+        productNumber: '1677',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+]
